6b3a632c0fb2116166ddea212df8ee32ee4972d2
GuilhermeTrivilin <trivilin.dev@hotmail.com>
2020-08-23 22:35:00 -0300

    Adicionando footer na tela de servicos finalizados

--- src/pages/Main/OrdersFinalized/index.js
+++ src/pages/Main/OrdersFinalized/index.js
@@ -3,8 +3,14 @@ import { View, StyleSheet, Text } from 'react-native'
 
 import Header from '~/components/Header'
 import FinalizedOrderCard from './components/FinalizedOrderCard'
+import RenderStars from '~/components/RenderStars'
+
+import { shadow } from '~/commons'
+import { useSafeArea } from 'react-native-safe-area-context'
+
 
 export default function OrdersFinalized({ navigation, route }) {
+    const insets = useSafeArea()
 
     return <>
         <Header
@@ -30,11 +36,34 @@ export default function OrdersFinalized({ navigation, route }) {
                 value={100}
             />
         </View>
+
+        <View style={[styles.footer, shadow, { paddingBottom: insets.bottom }]}>
+            <Text style={styles.footerText}>Média nos atendimentos</Text>
+            <View style={styles.verticalDivider} />
+            <RenderStars stars_length={3} />
+        </View>
     </>
 }
 
 const styles = StyleSheet.create({
-    container: {
-
+    footer: {
+        backgroundColor: '#FFF',
+        width: '100%',
+        paddingVertical: 10,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        bottom: 0
+    },
+    verticalDivider: {
+        width: 1,
+        height: '95%',
+        backgroundColor: 'gray',
+        marginHorizontal: 10
+    },
+    footerText: {
+        fontWeight: 'bold',
+        fontSize: 18
     }
 })
