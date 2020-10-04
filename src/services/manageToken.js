4e4f9af9edaed79cdc2cdf645157278c890746a8
Guilherme Trivilin <guilherme.pimentel@flexait.com.br>
2020-10-04 17:21:49 -0300

    Mudando nome da key do async storage que salva o token

--- src/services/manageToken.js
+++ src/services/manageToken.js
@@ -1,14 +1,14 @@
 import AsyncStorage from '@react-native-community/async-storage';
 
 export const saveToken = async (token) => {
-    await AsyncStorage.setItem('@welldelivery_app/token', token)
+    await AsyncStorage.setItem('@disk_ello_partners/token', token)
 }
 
 export const getToken = async () => {
-    const token = await AsyncStorage.getItem('@welldelivery_app/token')
+    const token = await AsyncStorage.getItem('@disk_ello_partners/token')
     return token
 }
 
 export const deleteToken = async () => {
-    await AsyncStorage.removeItem('@welldelivery_app/token')
+    await AsyncStorage.removeItem('@disk_ello_partners/token')
 }
