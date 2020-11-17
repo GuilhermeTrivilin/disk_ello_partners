4c77021bc0637d50e1de1e8da83936b0413fe7f7
GuilhermeTrivilin <guilherme.pimentel@flexait.com.br>
2020-11-17 20:25:15 -0300

    fix

--- src/pages/Main/OrdersWaiting/index.js
+++ src/pages/Main/OrdersWaiting/index.js
@@ -15,7 +15,7 @@ export default function OrdersWaiting({ navigation, route }) {
 
     const orders = async () => {
         const response = await getWaitingOrders()
-        setWaitingOrdersList(response.filter(item => item.id == 43))
+        setWaitingOrdersList(response)
     }
 
     const handleAcceptOrder = async (order_id) => {
